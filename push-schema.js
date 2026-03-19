0de390cb3d754e31d9eb7151e03b1865f7d3139b
JD Davenport <jddavenport46@gmail.com>
2026-03-19 00:08:28 -0600

    fix: remove hardcoded database credentials, use env var

--- push-schema.js
+++ push-schema.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:REDACTED_PASSWORD@db.vgdyrswcuxcjmpwebytv.supabase.co:6543/postgres'
+  connectionString: process.env.DATABASE_URL
 });
 
 const sql = `
